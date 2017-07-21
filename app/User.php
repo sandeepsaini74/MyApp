c25909da80055e328ddd0ab9627ccd7c0fa7d26c
Sandeep <Sandeepcs336@gmail.com>
2017-07-22 00:14:28 +0530

    users with uploading feature working

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','role_id','photo_id','is_active'
     ];
 
     /**
@@ -29,5 +29,17 @@ class User extends Authenticatable
 
         return $this->belongsTo('App\Role');
     }
+
+
+
+    public function photo(){
+
+        return $this->belongsTo('App\Photo');
+
+    }
+
+
+
+
 
 }
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -35,8 +35,8 @@
         </div>
 
         <div class="form-group">
-           {!! Form::label('file','Name ') !!}
-           {!! Form::file('file',null,['class'=>'form-control']) !!}
+           {!! Form::label('photo_id','Photo ') !!}
+           {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
         </div>
 
 
